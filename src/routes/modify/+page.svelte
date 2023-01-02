a44729b8bd8f49849c8878b0c50d19444bb43f12
snollygolly <snollygolly@gmail.com>
2023-01-02 14:35:45 -0600

    Update +page.svelte

--- src/routes/modify/+page.svelte
+++ src/routes/modify/+page.svelte
@@ -6,13 +6,15 @@
 <div class="text-column">
 	<h1>Save Modification</h1>
 
+	<h3>This page is read-only for now, export does not work</h3>
+
 	<p>
 		Here you will see an overview on the stats and items we are able to modify for your save files. Support for additional features coming soon. When you have finished modifying your save files how you want, click on "Export" in the menu to go to the next step. 
 	</p>
 
 	<div>
 		{#if mergedSave.save}
-		<DataTable table$aria-label="People list" style="max-width: 100%;">
+		<DataTable table$aria-label="Basic Bio Stats" style="width: 100%;">
 			<Head>
 			  <Row>
 				<Cell>Property</Cell>
@@ -38,6 +40,27 @@
 				</Row>
 			</Body>
 		  </DataTable>
+		  <hr>
+		  <canvas height="400" width="400"></canvas>
+		  <hr>
+		  <DataTable table$aria-label="Inventory" style="width: 100%;" bind:this={testbind}>
+			<Head>
+			  <Row>
+				<Cell>ID</Cell>
+				<Cell numeric>Qty</Cell>
+				<Cell>Name</Cell>
+			  </Row>
+			</Head>
+			<Body>
+				{#each mergedSave.save.Inventory as item}
+				<Row>
+					<Cell>{item.id}</Cell>
+					<Cell numeric>{item.qty}</Cell>
+					<Cell>{data.items[item.id].name}</Cell>
+				</Row>
+				{/each}
+			</Body>
+		  </DataTable>
 		  {/if}
 	</div>
 </div>
@@ -45,12 +68,28 @@
 <script>
 	import DataTable, { Head, Body, Row, Cell } from '@smui/data-table';
 	import { browser } from "$app/environment";
+	import { onMount } from 'svelte';
 
 	export let data;
+	let testbind;
 	let mergedSave = {};
-	if (browser) {
-		mergedSave = JSON.parse(localStorage.getItem("jsSave"));
-	}	
-	console.log("modify page load, merged save:")
-	console.log(mergedSave)
+
+	onMount(() => {
+		const canvas = document.querySelector("canvas");
+		console.log("test canvas", canvas)
+		console.log("table here?", testbind)
+		if (browser) {
+			mergedSave = JSON.parse(localStorage.getItem("jsSave"));
+			//draw the inventory image
+			//if (canvas.getContext) {
+				const ctx = canvas.getContext("2d");
+				for (const item of mergedSave.save.Inventory) {
+					ctx.fillRect(item.x, item.y, 20, 20);
+				}
+			//}
+		}	
+		console.log("modify page load, merged save:")
+		console.log(mergedSave)
+	});
+	
 </script>
